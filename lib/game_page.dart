497e2bf667df30d10689c11e98e1737b8fb43bd7
Nickolas <nickolas.chong@mediacorp.com.sg>
2021-06-05 19:42:41 +0800

    Clean up buildLanes code

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_ball_or_bomb/ball_or_bomb.dart';
 
 class GamePage extends StatefulWidget {
   const GamePage({Key? key}) : super(key: key);
@@ -12,6 +13,7 @@ class GamePage extends StatefulWidget {
 class _GamePageState extends State<GamePage> {
   late double topPosition;
   late double leftPosition;
+  late double laneSize;
 
   double generateTopPosition(double top) => Random().nextDouble() * top;
 
@@ -23,53 +25,51 @@ class _GamePageState extends State<GamePage> {
 
   void changePosition(double top) {
     setState(() {
-      topPosition = generateTopPosition(top);
+      topPosition = generateTopPosition(500);
     });
   }
+  // TODO: Auto animate position from top to bottom
 
   @override
   Widget build(BuildContext context) {
-    final appBar = AppBar(
-      title: const Text('BALL ⚽ or BOMB 💣'),
-    );
+    laneSize = MediaQuery.of(context).size.width / 3;
+
     return Scaffold(
-      appBar: appBar,
+      appBar: AppBar(
+        title: const Text('BALL ⚽ or BOMB 💣'),
+      ),
       body: Row(
         children: [
           buildLane(Colors.grey[200]),
           buildLane(Colors.grey[100]),
-          buildLane(Colors.grey[50]),
-
-          // Expanded(
-          //   child: Stack(
-          //     children: [
-          //       Container(color: Colors.grey[100]),
-          //       AnimatedPositioned(
-          //         width: size.width / 3,
-          //         height: size.width / 3,
-          //         top: topPosition,
-          //         left: 0,
-          //         duration: const Duration(seconds: 1),
-          //         child: InkWell(
-          //           onTap: () => changePosition(
-          //               size.height - (appBar.preferredSize.height + 50)),
-          //           child: const BallOrBomb(),
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          // Expanded(
-          //   child: Container(color: Colors.grey[50]),
-          // )
+          buildLane3(Colors.grey[50]),
         ],
       ),
     );
   }
 
-  Expanded buildLane(Color? color) {
-    // final size = MediaQuery.of(context).size;
+  Widget buildLane3(Color? color) {
+    return Expanded(
+      child: Stack(
+        children: [
+          Container(color: color),
+          AnimatedPositioned(
+            top: topPosition,
+            left: 0,
+            duration: const Duration(seconds: 1),
+            child: InkWell(
+              onTap: () => changePosition(50),
+              child: BallOrBomb(
+                size: laneSize,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
+  Widget buildLane(Color? color) {
     return Expanded(
       child: Stack(
         children: [
