27604448a83ec119aa622e3e4c6401e8ec106524
Nickolas <nickolas.chong@mediacorp.com.sg>
2021-06-09 22:12:51 +0800

    Enable wakelock for Android & iOS only

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_ball_or_bomb/game_page.dart';
 import 'package:wakelock/wakelock.dart';
@@ -9,7 +11,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Wakelock.enable();
+    if (Platform.isAndroid || Platform.isIOS) {
+      Wakelock.enable();
+    }
 
     return MaterialApp(
       title: 'BALL ⚽ or BOMB 💣',
