95c9828918caba1a19c12ac23e6aaccf74f64381
Nickolas <nickolas.chong@mediacorp.com.sg>
2021-06-15 21:07:46 +0800

    Set preferred Potrait orientation and remove Debug Banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_ball_or_bomb/game_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wakelock/wakelock.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(ProviderScope(child: MyApp()));
 }
 
@@ -13,11 +15,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (Platform.isAndroid || Platform.isIOS) {
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+      ]);
       Wakelock.enable();
     }
 
     return MaterialApp(
       title: 'BALL ⚽ or BOMB 💣',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.brown,
       ),
