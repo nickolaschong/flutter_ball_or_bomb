15e834bdf8511b9fcddf19678ae9082dffe478d8
Nickolas <nickolas.chong@mediacorp.com.sg>
2021-06-16 23:15:26 +0800

    Convert from AnimatedPosition to using AnimationController

--- lib/game_lane.dart
+++ lib/game_lane.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_ball_or_bomb/ball_or_bomb.dart';
 import 'package:flutter_ball_or_bomb/game_state.dart';
@@ -8,26 +10,40 @@ class GameLane extends StatefulWidget {
     Key? key,
     required this.color,
     required this.size,
-    required this.lowerBound,
     required this.upperBound,
   }) : super(key: key);
 
   final Color? color;
   final double size;
-  final double lowerBound;
   final double upperBound;
 
   @override
   _GameLaneState createState() => _GameLaneState();
 }
 
-class _GameLaneState extends State<GameLane> {
-  late double position;
+class _GameLaneState extends State<GameLane>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
 
   @override
   void initState() {
-    position = widget.lowerBound;
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+      upperBound: widget.upperBound,
+    );
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -36,9 +52,9 @@ class _GameLaneState extends State<GameLane> {
       provider: gameStateProvider,
       onChange: (context, state) {
         state.maybeWhen(
-          start: () => position += 1,
-          stop: () => position = position,
-          orElse: () => position = widget.lowerBound + 1,
+          start: () => _controller.repeat(),
+          stop: () => _controller.reset(),
+          orElse: () => _controller.reset(),
         );
       },
       child: Consumer(
@@ -48,20 +64,21 @@ class _GameLaneState extends State<GameLane> {
           return Stack(
             children: [
               child!,
-              AnimatedPositioned(
-                onEnd: () {
-                  if (state == const GameState.stop()) return;
-                  setState(() {
-                    position = position == widget.upperBound
-                        ? widget.lowerBound
-                        : widget.upperBound;
-                  });
-                },
-                // curve: Curves.linear,
-                top: position,
-                duration: const Duration(milliseconds: 1500),
-                child: BallOrBomb(
-                  size: state == const GameState.stop() ? 0 : widget.size,
+              Positioned(
+                top: _controller.value,
+                child: Listener(
+                  onPointerDown: (e) {
+                    setState(() {
+                      _controller.reset();
+                      Future.delayed(
+                          Duration(milliseconds: Random().nextInt(50)), () {
+                        _controller.repeat();
+                      });
+                    });
+                  },
+                  child: BallOrBomb(
+                    size: state == const GameState.stop() ? 0 : widget.size,
+                  ),
                 ),
               ),
             ],
@@ -72,3 +89,83 @@ class _GameLaneState extends State<GameLane> {
     );
   }
 }
+
+// class GameLane extends StatefulWidget {
+//   const GameLane({
+//     Key? key,
+//     required this.color,
+//     required this.size,
+//     required this.lowerBound,
+//     required this.upperBound,
+//   }) : super(key: key);
+//
+//   final Color? color;
+//   final double size;
+//   final double lowerBound;
+//   final double upperBound;
+//
+//   @override
+//   _GameLaneState createState() => _GameLaneState();
+// }
+//
+// class _GameLaneState extends State<GameLane> {
+//   late double _position;
+//   late int _speedInMs;
+//
+//   @override
+//   void initState() {
+//     _position = widget.lowerBound;
+//     _speedInMs = 1500;
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return ProviderListener<GameState>(
+//       provider: gameStateProvider,
+//       onChange: (context, state) {
+//         state.maybeWhen(
+//           start: () => _position += 1,
+//           stop: () => _position = _position,
+//           orElse: () => _position = widget.lowerBound + 1,
+//         );
+//       },
+//       child: Consumer(
+//         builder: (context, watch, child) {
+//           final state = watch(gameStateProvider);
+//
+//           return Stack(
+//             children: [
+//               child!,
+//               AnimatedPositioned(
+//                 onEnd: () {
+//                   if (state == const GameState.stop()) return;
+//                   setState(() {
+//                     _position = _position == widget.upperBound
+//                         ? widget.lowerBound
+//                         : widget.upperBound;
+//                     _speedInMs = 2000;
+//                   });
+//                 },
+//                 top: _position,
+//                 duration: Duration(milliseconds: _speedInMs),
+//                 child: Listener(
+//                   onPointerDown: (e) {
+//                     setState(() {
+//                       _speedInMs = 1;
+//                       _position = widget.lowerBound;
+//                     });
+//                   },
+//                   child: BallOrBomb(
+//                     size: state == const GameState.stop() ? 0 : widget.size,
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           );
+//         },
+//         child: Container(color: widget.color),
+//       ),
+//     );
+//   }
+// }
