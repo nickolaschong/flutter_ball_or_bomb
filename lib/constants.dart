af9ab809088f391e2ba70dfcc26faa5e6ba6484b
Nickolas <nickolas.chong@mediacorp.com.sg>
2021-06-19 22:59:01 +0800

    Added contants file for easy tweaking

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color bombColor = Colors.black;
+const List<Color> ballColors = [
+  Colors.pink,
+  Colors.indigo,
+  Colors.cyan,
+  Colors.amber,
+  Colors.lime
+];
+const int bombChancePercentage = 20;
