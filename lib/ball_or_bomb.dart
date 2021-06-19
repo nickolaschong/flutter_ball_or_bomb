92aa67cd88d547360d5a179f91ede24eea29eb60
Nickolas <nickolas.chong@mediacorp.com.sg>
2021-06-19 22:56:51 +0800

    Convert into stateless widget

--- lib/ball_or_bomb.dart
+++ lib/ball_or_bomb.dart
@@ -1,65 +1,12 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-class BallOrBomb extends StatefulWidget {
-  const BallOrBomb({Key? key, required this.size}) : super(key: key);
-  final double size;
-
-  @override
-  _BallOrBombState createState() => _BallOrBombState();
-}
-
-class _BallOrBombState extends State<BallOrBomb> {
-  late Color _currentColor;
-  final Color _bombColor = Colors.black;
-  final List<Color> _ballColors = [
-    Colors.pink,
-    Colors.indigo,
-    Colors.cyan,
-    Colors.amber,
-    Colors.lime
-  ];
-  late double size;
-
-  final _random = Random();
-  final int _bombChancePercentage = 10;
-
-  @override
-  void initState() {
-    _currentColor = randomColor();
-    size = widget.size;
-    super.initState();
-  }
-
-  Color randomColor() => _ballColors[_random.nextInt(_ballColors.length)];
-
-  void randomBallOrBomb() {
-    setState(() {
-      _currentColor = _random.nextInt(99) + 1 > _bombChancePercentage
-          ? randomColor()
-          : _bombColor;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return CircularContainer(
-      size: widget.size,
-      color: _currentColor,
-      onTap: randomBallOrBomb,
-    );
-  }
-}
-
-class CircularContainer extends StatelessWidget {
-  const CircularContainer({
+class BallOrBomb extends StatelessWidget {
+  const BallOrBomb({
     Key? key,
     required this.size,
-    required this.onTap,
     required this.color,
+    required this.onTap,
   }) : super(key: key);
-
   final double size;
   final VoidCallback onTap;
   final Color color;
