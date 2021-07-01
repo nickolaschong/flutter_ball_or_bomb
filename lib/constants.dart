4a1ffa02eb96e459ef2891aa9b5dd50687d349b1
Nickolas <nickolas.chong@mediacorp.com.sg>
2021-07-01 22:18:58 +0800

    Added Game Timer

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+const Duration gameDuration = Duration(seconds: 10);
 const Color bombColor = Colors.black;
 const List<Color> ballColors = [
   Colors.pink,
---
+++ lib/game_timer.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_ball_or_bomb/constants.dart';
+import 'package:flutter_ball_or_bomb/game_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class GameTimer extends StatefulWidget {
+  const GameTimer({Key? key}) : super(key: key);
+
+  @override
+  _GameTimerState createState() => _GameTimerState();
+}
+
+class _GameTimerState extends State<GameTimer> with TickerProviderStateMixin {
+  late final Ticker _ticker;
+  late AnimationController _controller;
+  late Animation<Color?> _animation;
+
+  Duration _elapsed = Duration.zero;
+  int get _timeLeft => (gameDuration - _elapsed).inSeconds;
+
+  @override
+  void initState() {
+    _ticker = createTicker((elapsed) {
+      setState(() {
+        _elapsed = elapsed;
+        if (_timeLeft == 0) {
+          context.read(gameStateProvider.notifier).stop();
+        }
+      });
+    });
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: gameDuration,
+    );
+
+    _animation =
+        ColorTween(begin: Colors.green, end: Colors.red).animate(_controller)
+          ..addListener(() {
+            setState(() {});
+          });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _ticker.dispose();
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ProviderListener<GameState>(
+      provider: gameStateProvider,
+      onChange: (context, state) {
+        state.maybeWhen(
+          start: () {
+            _ticker.start();
+            _controller.reset();
+            _controller.forward();
+          },
+          stop: () {
+            _ticker.stop();
+            _controller.stop();
+          },
+          orElse: () {
+            _ticker.stop();
+            _controller.stop();
+          },
+        );
+      },
+      child: Container(
+        color: _animation.value,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Text(
+              'Time left: $_timeLeft',
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
