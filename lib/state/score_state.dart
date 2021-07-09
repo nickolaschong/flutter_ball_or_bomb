093263af4dd2fe75b289fa30206571d16676cc4d
Nickolas <nickolas.chong@mediacorp.com.sg>
2021-07-09 18:01:22 +0800

    Updated App Name

--- lib/state/score_state.dart
+++ lib/state/score_state.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final scoreStateProvider =
-    StateNotifierProvider<ScoreNotifier, int>((ref) => ScoreNotifier());
+final scoreStateProvider = StateNotifierProvider<ScoreNotifier, int>((ref) {
+  return ScoreNotifier();
+});
 
 class ScoreNotifier extends StateNotifier<int> {
   ScoreNotifier() : super(0);
